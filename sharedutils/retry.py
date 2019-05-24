5efed7e8a37c0ea175d59b18f1cd8a0c5376e9b2
user <user@pdfcrowd-dev.localnet>
2019-05-24 08:58:08 +0000

    python 2/3 compatibility

--- sharedutils/retry.py
+++ sharedutils/retry.py
@@ -1,8 +1,12 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import print_function
+
 import datetime
 import django_rq
 from functools import wraps
 from rq import get_current_job
-import config
+from . import config
 
 RQ_RETRY_DEFAULT_BACKOFF = 60*60
 
